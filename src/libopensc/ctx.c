0b31a3b6e4713389ac34f0633c70a146b0739a31
Tadas Sasnauskas <tadas@yoyo.lt>
2026-01-29 18:37:02 +0200

    Driver list - move lteid up. APDU transactions are not used to detect.

--- src/libopensc/ctx.c
+++ src/libopensc/ctx.c
@@ -138,6 +138,8 @@ static const struct _sc_driver_entry internal_card_drivers[] = {
 #if defined(ENABLE_SM) && defined(ENABLE_OPENPACE)
 	{ "edo",        (void *(*)(void)) sc_get_edo_driver },
 #endif
+	{ "lteid", (void *(*)(void)) sc_get_lteid_driver },
+
 
 /* Here should be placed drivers that need some APDU transactions in the
  * driver's `match_card()` function. */
@@ -165,7 +167,6 @@ static const struct _sc_driver_entry internal_card_drivers[] = {
 #if defined(ENABLE_SM) && defined(ENABLE_OPENPACE)
 	{ "eOI",	(void *(*)(void)) sc_get_eoi_driver },
 #endif
-	{ "lteid", (void *(*)(void)) sc_get_lteid_driver },
 	/* The default driver should be last, as it handles all the
 	 * unrecognized cards. */
 	{ "default",	(void *(*)(void)) sc_get_default_driver },
