bffcf145e95242ac947f61d4959119074d321e20
Tadas Sasnauskas <tadas@yoyo.lt>
2026-02-07 15:04:52 +0200

    Another "I have no idea what I'm doing" change
    Pin authentication works via the PACE mechanism. But I do not know if it's
    anywhere near reasonable way to do that.

--- src/libopensc/card-lteid.c
+++ src/libopensc/card-lteid.c
@@ -39,6 +39,11 @@ static struct sc_card_driver lteid_drv = {
 	NULL, 0, NULL
 };
 
+struct lteid_drv_data {
+	unsigned char pace;
+	unsigned char pace_pin_ref;
+};
+
 static const struct sc_atr_table lteid_atrs[] = {
 	{ "3b:9d:18:81:31:fc:35:80:31:c0:69:4d:54:43:4f:53:73:02:06:05:d0", NULL, NULL, SC_CARD_TYPE_LTEID, 0, NULL },
 	{ NULL, NULL, NULL, 0, 0, NULL }
@@ -64,7 +69,7 @@ static int lteid_get_can(sc_card_t* card, struct establish_pace_channel_input* p
 
 	can = getenv("LTEID_CAN");
 
-	if (!can || can[0] != '\0') {
+	if (!can || can[0] == '\0') {
 		for (size_t i = 0; card->ctx->conf_blocks[i]; ++i) {
 			scconf_block** blocks = scconf_find_blocks(card->ctx->conf, card->ctx->conf_blocks[i], "card_driver", "lteid");
 			if (!blocks)
@@ -88,21 +93,47 @@ static int lteid_get_can(sc_card_t* card, struct establish_pace_channel_input* p
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
 
-static int lteid_unlock(sc_card_t* card) {
+// Mostly taken from `dtrust_perform_pace`
+static int lteid_perform_pace(struct sc_card *card, const int ref, const unsigned char *pin, size_t pinlen, int *tries_left) {
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 
+	struct lteid_drv_data *drv_data = card->drv_data;
 	struct establish_pace_channel_input pace_input = {0};
 	struct establish_pace_channel_output pace_output = {0};
 
-	if (SC_SUCCESS != lteid_get_can(card, &pace_input)) {
-		sc_log(card->ctx, "Error reading CAN.");
+	if (drv_data->pace && drv_data->pace_pin_ref != ref) {
+		sc_log(card->ctx, "Re-opening PACE with pin ref 0x%02x. Previous pin ref: 0x%02x.", ref, drv_data->pace_pin_ref);
+		sc_sm_stop(card);
+	}
+
+	if (ref == PACE_PIN_ID_CAN) {
+		if (SC_SUCCESS != lteid_get_can(card, &pace_input)) {
+			LOG_FUNC_RETURN(card->ctx, SC_ERROR_UNKNOWN);
+		}
+	} else {
+		pace_input.pin_id = ref;
+		pace_input.pin = pin;
+		pace_input.pin_length = pinlen;
+	}
+
+	// FIXME: figure out error handling for incorrect pin. Otherwise connection is irrecoverably broken.
+	if (SC_SUCCESS != perform_pace(card, pace_input, &pace_output, EAC_TR_VERSION_2_02)) {
+		sc_log(card->ctx, "Error performing PACE for pin ref 0x%02x.", ref);
 		LOG_FUNC_RETURN(card->ctx, SC_ERROR_UNKNOWN);
 	}
 
-	// FIXME: This should possibly go to lteid_sm_open. See card-eoi.c for example.
-	// Otherwise, in opensc_explorer it's possible to "sm close", but "sm open" fails with "Not supported".
-	if (SC_SUCCESS != perform_pace(card, pace_input, &pace_output, EAC_TR_VERSION_2_02)) {
-		sc_log(card->ctx, "Error verifying CAN.");
+	// Track PACE status
+	drv_data->pace = 1;
+	drv_data->pace_pin_ref = ref;
+
+	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
+}
+
+static int lteid_unlock(sc_card_t* card) {
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	// if (SC_SUCCESS != lteid_perform_pace(card, PACE_PIN_ID_PIN, NULL, 0, NULL)) {
+	if (SC_SUCCESS != lteid_perform_pace(card, PACE_PIN_ID_CAN, NULL, 0, NULL)) {
 		LOG_FUNC_RETURN(card->ctx, SC_ERROR_UNKNOWN);
 	}
 
@@ -111,6 +142,15 @@ static int lteid_unlock(sc_card_t* card) {
 
 static int lteid_init(sc_card_t* card) {
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	struct lteid_drv_data *drv_data = calloc(1, sizeof(struct lteid_drv_data));
+
+	if (drv_data == NULL)
+		LOG_FUNC_RETURN(card->ctx, SC_ERROR_OUT_OF_MEMORY);
+
+	drv_data->pace = 0;
+	drv_data->pace_pin_ref = 0;
+	card->drv_data = drv_data;
 
 	memset(&card->sm_ctx, 0, sizeof card->sm_ctx);
 
@@ -129,7 +169,63 @@ static int lteid_logout(sc_card_t* card) {
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 
 	sc_sm_stop(card);
-	return lteid_unlock(card);
+
+	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
+}
+
+static int lteid_pin_cmd(struct sc_card *card, struct sc_pin_cmd_data *data, int *tries_left) {
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	struct lteid_drv_data *drv_data = card->drv_data;
+	int rv;
+
+	sc_log(card->ctx, "== lteid_pin_cmd ===============================");
+	sc_log(card->ctx, "PIN cmd: %i", data->cmd);
+	sc_log(card->ctx, "PIN reference: %i", data->pin_reference);
+	sc_log(card->ctx, "PIN type: %i", data->pin_type);
+	if (data->pin1.data != NULL && data->pin1.len == 6) {
+		sc_log(card->ctx, "PIN: '%.6s'", data->pin1.data);
+	}
+	sc_log(card->ctx, "================================================");
+
+	// FIXME: Limit to PACE pin references only?.. There's the 0x81 pin which is not PACE pin.
+	if (data->cmd == SC_PIN_CMD_VERIFY) {
+		rv = lteid_perform_pace(card, data->pin_reference, data->pin1.data, data->pin1.len, tries_left);
+
+		LOG_FUNC_RETURN(card->ctx, rv);
+	}
+
+	if (data->cmd == SC_PIN_CMD_GET_INFO) {
+		// For now just pretend we got the information
+		// *tries_left = 3;
+		data->pin1.max_tries = 3;
+		data->pin1.tries_left = 3;
+
+		// Log in via Firefox PKCS11 module dialog works with this. Not sure yet how to log out.
+		data->pin1.logged_in = (drv_data->pace_pin_ref == data->pin_reference);
+
+		LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
+	}
+
+	rv = iso_ops->pin_cmd(card, data, tries_left);
+
+	LOG_FUNC_RETURN(card->ctx, rv);
+}
+
+static int lteid_set_security_env(struct sc_card *card, const struct sc_security_env *env, int se_num) {
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	int rv = iso_ops->set_security_env(card, env, se_num);
+
+	LOG_FUNC_RETURN(card->ctx, rv);
+}
+
+static int lteid_compute_signature(struct sc_card *card, const u8 * data, size_t data_len, u8 * out, size_t outlen) {
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	int rv = iso_ops->compute_signature(card, data, data_len, out, outlen);
+
+	LOG_FUNC_RETURN(card->ctx, rv);
 }
 
 struct sc_card_driver* sc_get_lteid_driver(void)
@@ -143,8 +239,9 @@ struct sc_card_driver* sc_get_lteid_driver(void)
 	lteid_ops.match_card = lteid_match_card;
 	lteid_ops.init = lteid_init;
 	// lteid_ops.select_file = lteid_select_file;
-	// lteid_ops.set_security_env = lteid_set_security_env;
-	// lteid_ops.compute_signature = lteid_compute_signature;
+	lteid_ops.set_security_env = lteid_set_security_env;
+	lteid_ops.compute_signature = lteid_compute_signature;
+	lteid_ops.pin_cmd = lteid_pin_cmd;
 	lteid_ops.logout = lteid_logout;
 
 	return &lteid_drv;
